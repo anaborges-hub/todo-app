256668dbebf3d56998b9a7e6cae7b0ee510e222e
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2022-12-12 19:10:36 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppHeader from './Components/AppHeader';
+import PageTitle from './Components/PageTitle';
+import style from './styles/modules/app.module.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <PageTitle>TODO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+        <AppHeader />
+      </div>
+    </div>
+  );
+}
+
+export default App;
