ef08c9c9e59fd666c450edb4ec8c25ae4686828c
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-01-04 20:54:08 +0000

    Fix Styled components

--- src/Components/AppContent/AppContent.styles.js
+++ src/Components/AppContent/AppContent.styles.js
@@ -1,27 +1,13 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-export const StyledContent = styled(motion.p)`
-  display: inline-block;
-  font-size: 1.6rem;
-  font-weight: 500;
-  color: var(--black-2);
-  text-align: center;
-  margin: 0 auto;
-  padding: 0.5rem 1rem;
-  border-radius: 8px;
-  background-color: var(--gray-2);
-  width: max-content;
-  height: auto;
-`;
-
 export const ContentWrapper = styled(motion.div)`
   background-color: var(--bg-2);
   padding: 2rem;
   border-radius: 12px;
-  @include smallDeviceSize {
+  /* @include smallDeviceSize {
     padding: 1.5rem;
-  }
+  } */
 `;
 
 export const StyledEmptyText = styled(motion.p)`
