e45dbd5962cf7bacbc419fb3bc5b07e59fd30f40
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-01-13 11:17:53 +0000

    #2 test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './App.tsx';
 import './styles/GlobalStyles.css';
 import '@fontsource/poppins';
 import '@fontsource/poppins/500.css';
