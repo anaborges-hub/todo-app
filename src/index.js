2e859f9c9400d61defbfe6a6a8d0ac3e3960a38a
Ana Borges <78922064+anaborges-hub@users.noreply.github.com>
2023-01-16 16:46:02 +0000

    #3 - rename app.js to app.tsx and fix import

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import './Styled/globalStyle';
-
 import '@fontsource/poppins';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
 import { store } from './app/store';
-
-import App from './App.js';
+import App from './App.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
